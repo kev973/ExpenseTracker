a01eca6eb19e8fdbb3a3916c011338c5d0359af2
KévinBENOIT <kev.br77@gmail.com>
2026-08-18 14:30:24 +0200

    updated Expense added User

--- src/main/java/benoit/kevin/expensetracker/domain/model/Expense.java
+++ src/main/java/benoit/kevin/expensetracker/domain/model/Expense.java
@@ -1,7 +1,8 @@
-package benoit.kevin.expensetracker.domain;
+package benoit.kevin.expensetracker.domain.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 import java.util.Objects;
@@ -9,7 +10,7 @@ import java.util.Objects;
 @Entity
 public class Expense {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String type;
